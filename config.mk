f2fada92c3c026e158e25fbb393afcc3b8006e43
Sven Roederer <devel-sven@geroedel.de>
2017-07-15 12:47:16 +0200

    packages: add variant for uplink w/o tunnel
    as there ist currently no way to dynamically select
    an uplink-provider

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 # default parameters for Makefile
 SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
-PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
+PACKAGES_LIST_DEFAULT=default notunnel tunnel-berlin backbone
 LEDE_SRC=https://git.lede-project.org/source.git
 LEDE_COMMIT=ceea0ac25d987de1f84376405d38d43337d31cf5
 MAKE_ARGS=
